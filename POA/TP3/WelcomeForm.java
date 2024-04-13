1fe21f7dfe6fd5c31330872feea92675f9e6b182
KhalidRouissi1 <khalidrouissi7@gmail.com>
2024-04-13 12:31:57 +0100

    Fix things

--- POA/TP3/WelcomeForm.java
+++ POA/TP3/WelcomeForm.java
@@ -1,4 +1,4 @@
-package application;
+package com.example.demo;
 
 import javafx.application.Application;
 import javafx.application.Platform;
@@ -18,74 +18,94 @@ import javafx.scene.text.Font;
 import javafx.scene.text.FontWeight;
 import javafx.scene.text.Text;
 import javafx.stage.Stage;
+import javafx.scene.Group;
+import javafx.scene.shape.Circle;
+import javafx.scene.shape.Line;
+import javafx.scene.shape.Rectangle;
 
-public class WelcomeForm extends Application {
+public class WelcomeFrom extends Application {
     @Override
     public void start(Stage primaryStage) {
-        primaryStage.setTitle("JavaFX Welcome");
-        primaryStage.show();
 
+        Group root = new Group();
         GridPane grid = new GridPane();
         grid.setAlignment(Pos.CENTER);
         grid.setHgap(10);
         grid.setVgap(10);
-        grid.setPadding(new Insets(25, 25, 25, 25));
+        grid.setPadding(new Insets(25,25,25,25));
         Scene scene = new Scene(grid, 300, 275);
-        primaryStage.setScene(scene);
-
         Text scenetitle = new Text("Welcome");
-        scenetitle.setFont(Font.font("Tahoma", FontWeight.NORMAL, 20));
-        grid.add(scenetitle, 0, 0, 2, 1);
-
+        scenetitle.setFont(Font.font("Tahoma", FontWeight.NORMAL,20));
+        grid.add(scenetitle,0,0,2,1);
         Label userName = new Label("User Name:");
-        grid.add(userName, 0, 1);
-        TextField userTextField = new TextField();
-        grid.add(userTextField, 1, 1);
+        grid.add(userName,0,1);
 
+        TextField userTextField = new TextField();
+        grid.add(userTextField,1,1);
         Label pw = new Label("Password:");
         grid.add(pw, 0, 2);
         PasswordField pwBox = new PasswordField();
-        grid.add(pwBox, 1, 2);
+        grid.add(pwBox,1,2);
 
         Button btn = new Button("Sign in");
         HBox hbBtn = new HBox(10);
         hbBtn.setAlignment(Pos.BOTTOM_RIGHT);
-        
+        hbBtn.getChildren().add(btn);
+        grid.add(hbBtn,1,4);
 
-        Button exitBtn = new Button("Exit");
-        
-       
-        hbBtn.getChildren().addAll(btn,exitBtn);
-        grid.add(hbBtn, 1, 4);
-
-        final Text actiontarget = new Text();
-        grid.add(actiontarget, 1, 6);
+        final Text actiontarget =new Text();
+        grid.add(actiontarget,1,6);
 
         btn.setOnAction(new EventHandler<ActionEvent>() {
             @Override
-            public void handle(ActionEvent e) {
-            	String enteredPassword = pwBox.getText();
-                String correctPassword = "yassin"; 
+            public void handle(ActionEvent actionEvent) {
 
-                if (enteredPassword.equals(correctPassword)) {
+
+                String userNameText = userTextField.getText();
+                String password = pwBox.getText();
+
+                if ("Khaled".equals(userNameText) && "Rouissi".equals(password)) {
+                    actiontarget.setText("Sign in successful");
                     actiontarget.setFill(Color.GREEN);
-                    actiontarget.setText("hello "+userTextField.getText()+" your correct !");
+
                 } else {
+                    actiontarget.setText("Invalid username or password");
                     actiontarget.setFill(Color.FIREBRICK);
-                    actiontarget.setText("Mot de passe incorrect !");
+
                 }
+
             }
         });
 
+
+        Button exitBtn = new Button("Exit");
+        HBox exHbtn =new HBox(10);
+        exHbtn.setAlignment(Pos.BOTTOM_LEFT);
+        exHbtn.getChildren().add(exitBtn);
+        grid.add(exHbtn,2,4);
+
+
+
+        final Text exitTxt = new Text();
+        grid.add(exitTxt,1,6);
         exitBtn.setOnAction(new EventHandler<ActionEvent>() {
             @Override
-            public void handle(ActionEvent e) {
-                Platform.exit(); 
+            public void handle(ActionEvent actionEvent) {
+                Platform.exit();
             }
         });
-    }
 
+
+
+
+
+        primaryStage.setTitle("JavaFx Welcome");
+        primaryStage.setScene(scene);
+        primaryStage.show();
+
+    }
     public static void main(String[] args) {
         launch(args);
+
     }
 }
